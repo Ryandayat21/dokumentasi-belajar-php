1d086d3e265a5eb9ce243713aa61363681d56835
Ryan Hidayat <dayatryan32@gmail.com>
2026-09-08 14:29:28 +0700

    first commit

---
+++ pertemuan2/index.php
@@ -0,0 +1,57 @@
+<?php 
+// Pertemuan 2 - PHP Dasar
+// Sintaks PHP
+
+// Standar Output (echo, print, print_r, var_dump)
+
+// echo "Ryan Hidayat" ;
+// print "Teknik Informatika" ;
+// echo 2304130075;
+
+// Penulisan Sintaks PHP
+// 1. PHP di dalam HTML
+// 2. HTML di dalam PHP
+
+// Variabel dan Tipe Data
+// Variabel
+// tidak boleh diawali dengan angka, tapi boleh mengandung angka
+// $nama = "Ryan";
+// $umur = 22;
+// $ipk = 3.5;
+// $mahasiswa = true;
+// echo "Halo, nama saya $nama, umur saya $umur tahun, IPK saya $ipk, dan status mahasiswa saya adalah $mahasiswa.";
+
+// Operator
+// Aritmatika
+// + - * / %
+// Unary
+// + - ! ~
+// Increment / Decrement
+// ++ --
+//$x = 10;
+//$y = 20;
+//echo $x + $y;
+
+// Penggabungan String / Concatenation
+// .
+// $nama_depan = "Ryan";
+// $nama_belakang = "Pedrosa";
+// echo $nama_depan . " " . $nama_belakang;
+
+// Assignment
+// =, +=, -=, *=, /=, %=, .=
+$x = 10;
+$x .= 5;
+echo $x;
+
+// Perbandingan
+// <, >, <=, >=, ==, !=
+// tidak membandingkan tipe data, hanya nilai
+
+// Identitas
+// ===, !==
+// membandingkan tipe data dan nilai
+
+// Logika
+// &&, ||, !
+?>
