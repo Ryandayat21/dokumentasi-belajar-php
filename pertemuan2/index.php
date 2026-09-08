7f9c40402ebed21df3c578211aa794c25b003fa5
Ryan Hidayat <dayatryan32@gmail.com>
2026-09-08 14:42:40 +0700

    update pertemuan 2

--- pertemuan2/index.php
+++ pertemuan2/index.php
@@ -4,9 +4,9 @@
 
 // Standar Output (echo, print, print_r, var_dump)
 
-// echo "Ryan Hidayat" ;
-// print "Teknik Informatika" ;
-// echo 2304130075;
+echo "Ryan Hidayat<br>" ;
+print "Teknik Informatika<br>" ;
+echo "2304130075<br>";
 
 // Penulisan Sintaks PHP
 // 1. PHP di dalam HTML
@@ -15,11 +15,11 @@
 // Variabel dan Tipe Data
 // Variabel
 // tidak boleh diawali dengan angka, tapi boleh mengandung angka
-// $nama = "Ryan";
-// $umur = 22;
-// $ipk = 3.5;
-// $mahasiswa = true;
-// echo "Halo, nama saya $nama, umur saya $umur tahun, IPK saya $ipk, dan status mahasiswa saya adalah $mahasiswa.";
+$nama = "Ryan";
+$umur = 22;
+$ipk = 3.5;
+$mahasiswa = true;
+echo "Halo, nama saya $nama, umur saya $umur tahun, IPK saya $ipk, dan status mahasiswa saya adalah $mahasiswa.<br>";
 
 // Operator
 // Aritmatika
@@ -28,30 +28,36 @@
 // + - ! ~
 // Increment / Decrement
 // ++ --
-//$x = 10;
-//$y = 20;
-//echo $x + $y;
+$x = 10;
+$y = 20;
+echo ($x + $y) . "<br>";
 
 // Penggabungan String / Concatenation
 // .
-// $nama_depan = "Ryan";
-// $nama_belakang = "Pedrosa";
-// echo $nama_depan . " " . $nama_belakang;
+$nama_depan = "Ryan";
+$nama_belakang = "Pedrosa";
+echo $nama_depan . " " . $nama_belakang . "<br>";
 
 // Assignment
 // =, +=, -=, *=, /=, %=, .=
 $x = 10;
 $x .= 5;
-echo $x;
+echo $x . "<br>";
 
 // Perbandingan
 // <, >, <=, >=, ==, !=
 // tidak membandingkan tipe data, hanya nilai
+$a = 25;
+$b = "25";
+var_dump($a == $b) . "<br>"; 
 
 // Identitas
 // ===, !==
 // membandingkan tipe data dan nilai
+var_dump($a === $b) . "<br>";
 
 // Logika
 // &&, ||, !
+$c = 25;
+var_dump($a < 30 && $c > 20) . "<br>";
 ?>
