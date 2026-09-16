9f42ba79240de95c46a7ec7d62c8c5eba86650b7
Ryan Hidayat <dayatryan32@gmail.com>
2026-09-16 15:53:13 +0700

    Menambahkan fitur pengelolaan laptop: halaman indeks, tambah, dan edit - Membuat file index.php untuk menampilkan daftar laptop beserta opsi untuk mengedit dan menghapus.
    - Mengembangkan file ubah.php untuk mengedit detail laptop yang sudah ada, dengan mengisi formulir secara otomatis menggunakan data terkini.

---
+++ pertemuan11/index.php
@@ -0,0 +1,48 @@
+<?php
+require 'functions.php';
+$laptop = query("SELECT * FROM laptop");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Admin</title>
+</head>
+<body>
+    <h1>Daftar Laptop</h1>
+    <a href="tambah.php">Tambah Data Laptop</a>
+    <br> <br>
+
+    <table border="1" cellpadding="10" cellspacing="0">
+        <tr>
+            <th>No.</th>
+            <th>Aksi</th>
+            <th>Gambar</th>
+            <th>Merk</th>
+            <th>Tipe</th>
+            <th>Tahun Rilis</th>
+            <th>Harga</th>
+        </tr>
+
+        <?php $i = 1; ?>
+        <?php foreach( $laptop as $row) : ?>
+        <tr>
+            <td><?= $i; ?></td>
+            <td>
+                <a href="ubah.php?id=<?= $row["id"]; ?>">edit</a> |
+                <a href="hapus.php?id=<?= $row["id"]; ?>" 
+                onclick="return confirm('Yakin?');">hapus</a>
+            </td>
+            <td><img src="img/<?= $row["gambar"] ?>" width="100"></td>
+            <td><?= $row["merk"] ?></td>
+            <td><?= $row["tipe"] ?></td>
+            <td><?= $row["tahunRilis"] ?></td>
+            <td><?= $row["harga"] ?></td>
+        </tr>
+        <?php $i++; endforeach; ?>
+    </table>
+</body>
+</html>
---
+++ pertemuan11/ubah.php
@@ -0,0 +1,74 @@
+<?php
+require 'functions.php';
+
+// Ambil data di URL
+$id = $_GET["id"];
+
+// Query data laptop berdasarkan id
+$laptop = query("SELECT * FROM laptop WHERE id = $id")[0];
+
+// Cek apakah form telah disubmit
+if (isset($_POST["submit"])) {
+    
+    // Cek apakah data berhasil diubah atau tidak
+    if( edit($_POST) > 0 ) {
+        echo "
+            <script>
+                alert('Data Berhasil Diubah!');
+                document.location.href = 'index.php';
+            </script>";
+    } else {
+        echo "
+            <script>
+                alert('Data Gagal Diubah!');
+                document.location.href = 'index.php';
+            </script>";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah Data Laptop</title>
+</head>
+<body>
+    <h1>Edit Data Laptop</h1>
+
+    <form action="" method="post">
+        <input type="hidden" name="id" value="<?= $laptop["id"]; ?>">
+        <ul>
+            <li>
+                <label for="merk">Merk: </label>
+                <input type="text" name="merk" id="merk" required
+                value="<?= $laptop["merk"]; ?>">
+            </li>
+            <li>
+                <label for="tipe">Tipe: </label>
+                <input type="text" name="tipe" id="tipe" required
+                value="<?= $laptop["tipe"]; ?>">
+            </li>
+            <li>
+                <label for="tahunRilis">Tahun Rilis: </label>
+                <input type="text" name="tahunRilis" id="tahunRilis" required
+                value="<?= $laptop["tahunRilis"]; ?>">
+            </li>
+            <li>
+                <label for="harga">Harga: </label>
+                <input type="text" name="harga" id="harga" required
+                value="<?= $laptop["harga"]; ?>">
+            </li>
+            <li>
+                <label for="gambar">Gambar: </label>
+                <input type="text" name="gambar" id="gambar" required
+                value="<?= $laptop["gambar"]; ?>">
+            </li>
+            <li>
+                <button type="submit" name="submit">Ubah Data</button>
+            </li>
+        </ul>
+    </form>
+</body>
+</html>
